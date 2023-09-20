b6f43a327c1d40be3df2a8edecc3fa17a7a00652
Hugo <hugo.touron@etu.imt-nord-europe.fr>
2023-09-20 09:33:18 +0200

    echange contenu

--- TP-02-pointeurs/01-echangeContenu/main.c
+++ TP-02-pointeurs/01-echangeContenu/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "echangeContenu.h"
 
+
 int main(void)
 {
 	int a=4,b=14;
 
 	printf ("a = %d\tb = %d\n", a, b);
 
-	// TODO
+	echangeContenu(&a, &b);
 
 	printf ("a et b echanges \n");
 	printf ("a = %d\tb = %d\n", a, b);
