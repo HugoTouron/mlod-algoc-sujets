64755d29a2f043c3199341454adfcb4f6b45be67
Hugo <hugo.touron@etu.imt-nord-europe.fr>
2023-09-26 14:07:22 +0200

    toute petite modif

--- TP-02-pointeurs/04-MatriceMalloc/matrice.c
+++ TP-02-pointeurs/04-MatriceMalloc/matrice.c
@@ -8,5 +8,5 @@ Matrice* creer (int init, uint16_t nombreLignes, uint16_t nombreColonnes){
     Matrice *M=NULL;
     M->nbColonnes = nombreColonnes;
     M->nbLignes = nombreLignes;
-    M->valeurs = (int**)malloc();
+    M->valeurs = (int**)malloc(nombreLignes*nombreColonnes);
 }
