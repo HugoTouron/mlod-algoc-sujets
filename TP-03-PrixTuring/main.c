d65b5bcdec787ff7b40291877b8fa61f5ad1d6e1
Hugo <hugo.touron@etu.imt-nord-europe.fr>
2023-09-26 14:08:47 +0200

    debut TP3

---
+++ TP-03-PrixTuring/main.c
@@ -0,0 +1,28 @@
+/**
+ Compilation
+ gcc --std=c99 -W -Wall -o prixTuring prixTuring.c
+
+ Exécution
+ ./prixTuring
+
+ Tests
+ diff out.csv turingWinners.csv
+
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+
+
+int main(int argc, char** argv)
+{
+	char filename[] = "turingWinners.csv";
+	char outputFilename[] = "out.csv";
+
+    // TODO
+
+	return EXIT_SUCCESS;
+}
