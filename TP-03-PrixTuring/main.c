261d0011cf7e21f64843664c4771d7c7a9e499cf
Hugo <hugo.touron@etu.imt-nord-europe.fr>
2023-09-26 17:34:34 +0200

    turing

--- TP-03-PrixTuring/main.c
+++ TP-03-PrixTuring/main.c
@@ -15,6 +15,7 @@
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
+#include "prixTuring.h"
 
 
 int main(int argc, char** argv)
@@ -22,7 +23,13 @@ int main(int argc, char** argv)
 	char filename[] = "turingWinners.csv";
 	char outputFilename[] = "out.csv";
 
-    // TODO
+    FILE *fichier = fopen(filename, "r");
+	FILE *output = fopen(outputFilename, "w");
+
+	printf("%d", numberOfWinners(fichier));
+
+	fclose(output);
+	fclose(fichier);
 
 	return EXIT_SUCCESS;
 }
--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -1,31 +1,53 @@
-/**
- Compilation
- gcc --std=c99 -W -Wall -o prixTuring prixTuring.c
-
- Exécution
- ./prixTuring
-
- Tests
- diff out.csv turingWinners.csv
-
-**/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
+#include "prixTuring.h"
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// MAIN
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int numberOfWinners (FILE *fichier){
 
-int main(int argc, char** argv)
-{
-	char filename[] = "turingWinners.csv";
-	char outputFilename[] = "out.csv";
+    int compte = 0;
+    char ch;
 
-    // TODO
-
-	return EXIT_SUCCESS;
+    while((ch = fgetc(fichier)) != EOF){
+        if(ch == '\n'){
+            compte++;
+        }
+    }
+    return compte;
 }
+
+char buffer[512];
+
+char *readStringFromFileUntil(FILE *f, char delim){
+    char ch;
+    int i = 0;
+    memset(buffer, 0, sizeof(buffer));
+    while((ch = fgetc(f)) != delim){
+        buffer[i] = ch;
+        i++;
+    }
+    char* result = (char *)malloc(i + 2);
+    for(int k = 0; k < i; k++){
+        result[k] = buffer[k];
+    }
+    return result;
+}
+
+void readWinner(Gagnant *winner, FILE *f){
+    fscanf(f, "%" SCNd16 ";", &winner->annee);
+    winner->nom = readStringFromFileUntil(f,";");
+    winner->invention = readStringFromFileUntil(f,'\n');
+    
+}
+
+
+//cette fonction doit creer un gros malloc de tous les winners
+void readWinners(FILE *f){
+    for (int i = 0; i<numberOfWinners(f); i++){
+        Gagnant *winner;
+        readWinner(&winner, fgets(i, 1024, f));
+    }
+}
+
