776053dd4865c96a40a2974854e322f2418bc5c1
Hugo <hugo.touron@etu.imt-nord-europe.fr>
2023-10-04 12:03:42 +0200

    TP5

--- TP-05-arbre-bin/arbre-bin-recherche.c
+++ TP-05-arbre-bin/arbre-bin-recherche.c
@@ -4,47 +4,96 @@
 
 // retourne TRUE si a est l'arbre vide et FALSE sinon
 bool estVide(ArbreBinaire a) {
-	return true;
+	return a==NULL;
 }
 
 // initialise l'arbre a à l'arbre vide
 void initialiser(ArbreBinaire* a) {
+	*a = NULL;
 }
 
 // créer un arbre contenant un seul noeud ayant la valeur e
 ArbreBinaire creer(Element e) {
-
-	return NULL;
+	ArbreBinaire a;
+	a = (Noeud*)malloc(sizeof(Noeud));
+	a->filsDroit = NULL;
+	a->filsGauche = NULL;
+	a->val = e;
+	return a;
 }
 
 // insere e dans a sachant que a est un arbre binaire de recherche
 // si a contient déjà un element e, ce dernier n'est pas insérer afin d'éviter les doublons
 // version itérative
 ArbreBinaire insere_i(ArbreBinaire a, Element e) {
-
-	return NULL;
+	Noeud *courant = a;
+	if(estVide(a)){
+		a = creer(e);
+	}
+	else{
+		while(!estVide(courant)){
+			if(e > courant->val){
+				courant = courant->filsDroit;
+			}
+			else if (e < courant->val){
+				courant = courant->filsGauche;
+			}
+			else{
+				return a;
+			}
+		}
+		courant = creer(e);
+	}
+	return a;
 }	
 
 // insere e dans a sachant que a est un arbre binaire de recherche
 // si a contient déjà un element e, ce dernier n'est pas insérer afin d'éviter les doublons
 // version récursive
 ArbreBinaire insere_r(ArbreBinaire a, Element e) {
-	
-	return NULL;
+	if(estVide(a)){
+		return creer(e);
+	}
+	if(e > a->val){
+		a->filsDroit = insere_r(a->filsDroit, e);
+	}
+	if(e < a->val){
+		a->filsGauche = insere_r(a->filsGauche, e);
+	}
+	return a;
 }
 
 // retourne le nombre de noeud contenus dans l'arbre a
 int nombreDeNoeud(ArbreBinaire a){
-
+	if(estVide(a)){
 		return 0;
+	}
+	return (1 + nombreDeNoeud(a->filsDroit) + nombreDeNoeud(a->filsGauche));
 }
 
 
 // retourne la profondeur du noeud ayant la valeur e dans a
 // retourne -1 si a est vide ou si e n'est pas dans a
 int profondeur(ArbreBinaire a, Element e){
-
-		return 0;
+	Noeud *courant = a;
+	int compteur = 1;
+	if (estVide(a)){
+		return -1;
+	}
+	while(!estVide(courant)){
+		if(e > courant->val){
+			courant = courant->filsDroit;
+			compteur++;
+		}
+		else if (e < courant->val){
+			courant = courant->filsGauche;
+			compteur++;
+		}
+		else{
+			return compteur;
+		}
+	}
+	return -1;
 }
 
 // retourne la hauteur de l'arbre a
